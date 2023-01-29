0cf11c1e27f13470b739d44d105897218bc98af3
Ivonita <viskha.ivonita@gmail.com>
2023-01-29 21:46:29 +0100

    init

---
+++ 3.3.SplitTheBill.py
@@ -0,0 +1,13 @@
+#We are 5 people.
+#We have to pay 100€ for our dinner.
+#We want to split the bill.
+#We want to give 10€ tip.
+#How much everyone should pay?
+
+people = 5
+dinner = 100
+tip = 10
+
+cost_per_person = (dinner + tip) / people
+print(cost_per_person)
+
---
+++ demo.py
@@ -0,0 +1,48 @@
+# run using the button in the right hand side corner
+# use the terminal 'python3 demo.py'
+
+# define a variable, name
+msg = 'Hello'
+# print the content () on the terminal
+print(msg)
+
+my_name = 'Nathalie'
+my_name = 'Anna'
+# print the value of a variable
+print(my_name)
+
+# string
+print('Ivonita')
+
+# Number: Integer or Float
+my_age = 36
+
+# or Float
+temp = 2.2
+
+# <class 'int>
+print(type(my_age))
+
+## Calculate the year I was born:
+my_birthyear = 2023 - 36
+
+current_year = 2023
+my_birthyear = current_year - my_age
+print(my_birthyear)
+
+## Data type string, integer, float,
+#
+#boolean,
+mentor = True
+mentor = False
+
+# list,
+my_hobbies = ['running', 'coding', 'eating', 123, False, [1,2,3] ]
+
+# dictionary (dict)
+my_pet = {
+    'name': 'Susi',
+    'animal': 'cat',
+    'age': 2,
+    'cute': True,
+}
